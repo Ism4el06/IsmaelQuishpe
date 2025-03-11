47fc2cc1b2d4785ccf7be3c6a6a01c9ebf60fdcd
Isma <ismaarboleda2006@gmail.com>
2025-03-10 23:14:13 -0500

    8

--- arrays/bidimensional/definicion.cpp
+++ arrays/bidimensional/definicion.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 using namespace std;
-main()
-{
-    int tabla[3][3];//declarando
-    float pesos[2][3]={{1.2,3.4,5.6},{7.8,9.0,1.2}};//declarando e inicializando
-    cout<< pesos[0][0]<<endl;//imprimiendo el primer elemento de la primera frila y primera columna
-    cout <<pesos[1][1]<<endl;
-    for(int i=0;i<2;i++){
-        for(int j=0;j<3;j++){
-            cout<<pesos[i][j]<<"\t";
+
+int main() {
+    int tabla[3][3];
+    float pesos[2][3] = {{1.2, 3.4, 5.6}, {7.8, 9.0, 1.2}};
+
+    // Initialize tabla with some values
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            tabla[i][j] = i + j;
         }
-        cout<<endl;
     }
+
+    cout << pesos[0][0] << endl;
+    cout << pesos[1][1] << endl;
+
+    // Print tabla
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << tabla[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    // Print pesos
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << pesos[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }

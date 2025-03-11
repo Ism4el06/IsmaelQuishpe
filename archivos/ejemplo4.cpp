ef61098e43d37130cf316e509e3a5fc87d4771ba
Isma <ismaarboleda2006@gmail.com>
2025-03-10 23:08:17 -0500

    5

--- archivos/ejemplo4.cpp
+++ archivos/ejemplo4.cpp
@@ -1,19 +1,18 @@
-// Leer y escribir con un solo objeto
+
 #include <iostream>
 #include <fstream>
 #include <string>
 using namespace std;
 int main(){
-    // Crear un objeto de tipo fstream
+
     fstream archivo;
     string frase, nombreArchivo;
     cout << "Ingrese nombre de archivo con extension: ";
     cin >> nombreArchivo;
-    // Pedir al usuario una frase
+
     cout << "Escribe una frase para agregar al archivo: ";
     cin.ignore();
     getline(cin, frase);
-    // Abrir el archivo en modo escritura (append)
     archivo.open("ejemplo.txt", ios::out | ios::app);
     if(archivo.is_open()){
         archivo << frase << endl;
@@ -22,7 +21,7 @@ int main(){
         cout << "No se pudo abrir el archivo para escribir."<<endl;
         return 1;
     }
-    // Abrir el archivo en modo lectura
+
     archivo.open(nombreArchivo.c_str(), ios::in);
     if(archivo.is_open()){
         string linea;
